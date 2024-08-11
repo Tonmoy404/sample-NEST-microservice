1244c6f623ff949b8f9b98b7bbf1d0ef21f1a102
Tonmoy404 <tonmoy.spam6@gmail.com>
2024-08-11 13:17:24 +0600

    Added user service functions

--- user-service/src/app.controller.ts
+++ user-service/src/app.controller.ts
@@ -1,23 +1,47 @@
-import { Controller, Get } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from './app.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller()
+@Controller('users')
 export class UserController {
-  @MessagePattern({ cmd: 'get_users' })
-  getUsers() {
-    return [
-      {
-        id: 1,
-        name: 'Tonmoy',
-      },
-      {
-        id: 2,
-        name: 'Rifat',
-      },
-      {
-        id: 3,
-        name: 'Rabby',
-      },
-    ];
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAllUser() {
+    return this.userService.findAllUser();
+  }
+
+  @Post()
+  createUser(@Body(ValidationPipe) user: CreateUserDto) {
+    return this.userService.createUser(user);
+  }
+
+  @Get(':id')
+  findOneUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOneUser(id);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updatedUser: UpdateUserDto,
+  ) {
+    return this.userService.updateUser(id, updatedUser);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.deleteUser(id);
   }
 }
--- user-service/src/app.service.ts
+++ user-service/src/app.service.ts
@@ -1,8 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const hashedPassword = await this.hashPassword(createUserDto.password);
+    const newUser = this.userRepo.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    return this.userRepo.save(newUser);
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    const saltRounds = 9;
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    return await bcrypt.hash(password, salt);
+  }
+
+  async findAllUser() {
+    return await this.userRepo.find();
+  }
+
+  async findOneUser(id: number) {
+    const user = await this.userRepo.find({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User Not Found');
+    }
+
+    return user;
+  }
+
+  async updateUser(id: number, updatedUserDto: UpdateUserDto) {
+    const userToUpdate = await this.userRepo.find({ where: { id } });
+
+    if (!userToUpdate) {
+      throw new NotFoundException('User Does Not Exist');
+    }
+
+    Object.assign(userToUpdate, updatedUserDto);
+
+    return userToUpdate;
+  }
+
+  async deleteUser(id: number) {
+    const user = await this.userRepo.find({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User Not Found');
+    }
+
+    return this.userRepo.remove(user);
   }
 }
