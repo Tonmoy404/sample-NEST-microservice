f6799f6828ed48b01c8563e66512f0eeca347b1c
Tonmoy404 <tonmoy.spam6@gmail.com>
2024-08-11 12:04:18 +0600

    Added user entity

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.PORT,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.NAME,
+      entities: [__dirname + '/**/*.entity{.ts, .js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [UserController],
   providers: [AppService],
 })
